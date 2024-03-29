da7a9fc2836ccc19205717e3e6e25c3e90d8189d
Nathan <antnefedov33@gmail.com>
2024-03-29 12:46:26 +0300

    Merge Similar Items

---
+++ easy/Merge Similar Items.js
@@ -0,0 +1,8 @@
+// https://leetcode.com/problems/merge-similar-items/
+
+function mergeSimilarItems(i1, i2) {
+    let hash = {};
+    const countItems = (arr) => arr.forEach(([v, w]) => hash[v] === undefined ? (hash[v] = w) : (hash[v] += w));
+    countItems(i1); countItems(i2);
+    return Object.entries(hash).map(([v, w]) => [+v, w]).sort((a, b) => a[0] - b[0]);
+}
