65c2c7938392109d8fb14b3740993fda5a3ba5c3
Nathan <antnefedov33@gmail.com>
2024-05-17 10:19:25 +0300

    Permutations

---
+++ array/mediun/Permutations.js
@@ -0,0 +1,17 @@
+// https://leetcode.com/problems/permutations/description/
+
+function permute(arr) {
+    const res = [];
+    function p(c, r) {
+        if (r.length === 0) {
+            res.push(c);
+        } else {
+            for (let i = 0; i < r.length; i++) {
+                p([...c, r[i]], [...r.slice(0, i), ...r.slice(i + 1)]);
+            }
+        }
+    }
+
+    p([], arr);
+    return res;
+}
