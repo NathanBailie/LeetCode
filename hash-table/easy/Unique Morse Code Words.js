c9c3bf86828bc129c75f1975b8e7d150beb311f4
Nathan <antnefedov33@gmail.com>
2024-05-21 15:15:40 +0300

    Unique Morse Code Words

---
+++ hash-table/easy/Unique Morse Code Words.js
@@ -0,0 +1,13 @@
+// https://leetcode.com/problems/unique-morse-code-words/description/
+
+function uniqueMorseRepresentations(words) {
+    const hash = {
+        'a': ".-", 'b': "-...", 'c': "-.-.", 'd': "-..", 'e': ".", 'f': "..-.",
+        'g': "--.", 'h': "....", 'i': "..", 'j': ".---", 'k': "-.-", 'l': ".-..",
+        'm': "--", 'n': "-.", 'o': "---", 'p': ".--.", 'q': "--.-", 'r': ".-.",
+        's': "...", 't': "-", 'u': "..-", 'v': "...-", 'w': ".--", 'x': "-..-",
+        'y': "-.--", 'z': "--.."
+    };
+    const toMorse = word => Array.from(word, char => hash[char]).join('');
+    return new Set(words.map(toMorse)).size;
+}
